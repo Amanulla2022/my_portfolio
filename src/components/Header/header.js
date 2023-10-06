c0d8bedab7939f99817e539893b230a2816aeb87
Amanulla Mulla <amanmulla167@gmail.com>
2023-10-06 20:20:02 +0530

    header added

---
+++ src/components/Header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.css";
+import logo from "../../assets/logo.jpg";
+import cantactImg from "../../assets/contact.png";
+import { Link } from "react-scroll";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Client</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src={cantactImg} alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </header>
+  );
+};
+
+export default Header;
